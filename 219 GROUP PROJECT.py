c984db70c3e452d212f60801e025d13755f19c03
Rana-mansour <89980681+Rana-mansour@users.noreply.github.com>
2021-12-05 00:22:45 +0300

    Read csv file and clean data

---
+++ 219 GROUP PROJECT.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 13 21:39:29 2021
+
+@author: RANA
+"""
+# STEP 1: install csv and clean data
+
+import pandas as pd 
+poro_perm_data = pd.read_csv('poro_perm_data.csv')
+poro_perm_data.head()
+poro_perm_data.columns # shows column labels as series
+#poro_perm_data with negative, zero, and nan (not clean raw data)
+
+#for null values
+poro_perm_data.dropna(axis = 0, inplace = True)
+poro_perm_data.isnull().sum() #after cleaning Facies null values 
+    
