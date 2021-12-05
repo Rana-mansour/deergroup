893af641d0a3d68c95dff7310e41d548b56a9ceb
amroohaikal <89980808+amroohaikal@users.noreply.github.com>
2021-12-05 12:34:45 +0300

    Facies plots

--- 219 GROUP PROJECT.py
+++ 219 GROUP PROJECT.py
@@ -95,3 +95,24 @@ newCHdata= poro_perm_data[bool_las3]
 newCHdata.describe()
 #now min and max of each facies is identified
 
+#plots for channel 
+fig3 = poro_perm_data.plot(x='Depth (ft)', y='Porosity (%)',kind = 'scatter',xlabel = 'Depth (ft)', ylabel = 'porosity(%)', color='black', title= 'Channel facies porosity graph')
+plt.legend(['Depth (ft)','Porosity (%)'])
+
+fig4 = poro_perm_data.plot(x='Depth (ft)', y='Permeability (mD)',kind = 'scatter',xlabel = 'Depth (ft)', ylabel = 'Permeability (mD)', color='black', title= 'Channel facies permeability graph')
+plt.legend(['Depth (ft)','Permeability (mD)'])
+
+#plots for crevasse splay 
+fig5 = poro_perm_data.plot(x='Depth (ft)', y='Porosity (%)',kind = 'scatter',xlabel = 'Depth (ft)', ylabel = 'porosity(%)', color='black', title= 'crevasse splay facies porosity graph')
+plt.legend(['Depth (ft)','Porosity (%)'])
+
+fig6 = poro_perm_data.plot(x='Depth (ft)', y='Permeability (mD)',kind = 'scatter',xlabel = 'Depth (ft)', ylabel = 'Permeability (mD)', color='black', title= 'crevasse splay facies permeability graph')
+plt.legend(['Depth (ft)','Permeability (mD)'])
+
+#plots for overbanks
+fig7 = poro_perm_data.plot(x='Depth (ft)', y='Porosity (%)',kind = 'scatter',xlabel = 'Depth (ft)', ylabel = 'porosity(%)', color='black', title= 'overbanks facies porosity graph')
+plt.legend(['Depth (ft)','Porosity (%)'])
+
+fig8 = poro_perm_data.plot(x='Depth (ft)', y='Permeability (mD)',kind = 'scatter',xlabel = 'Depth (ft)', ylabel = 'Permeability (mD)', color='black', title= 'overbanks facies permeability graph')
+plt.legend(['Depth (ft)','Permeability (mD)'])
+
